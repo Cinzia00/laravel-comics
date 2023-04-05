484811f7511f18e9797f536f647d9e8e60a5b102
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-04-05 18:02:03 +0200

    Feat creati header e footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,144 @@
+<footer>
+        <div class="container">
+        </div>
+        <div class="main-footer">
+            <img class="big-logo" src="{{ Vite::asset ('resources/img/dc-logo-bg.png') }}" alt="">
+            <div class="container">
+                <div class="list-item">
+                    <ul>
+                        <li>
+                            <h3>DC COMICS</h3>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Comics</a>
+                        </li>
+                        <li>
+                            <a href="">Movies</a>
+                        </li>
+                        <li>
+                            <a href="">TV</a>
+                        </li>
+                        <li>
+                            <a href="">Games</a>
+                        </li>
+                        <li>
+                            <a href="">Videos</a>
+                        </li>
+                        <li>
+                            <a href="">News</a>
+                        </li>
+                    </li>
+                    <li>
+                        <h3>SHOP</h3>
+                    </li>
+                    <li>
+                        <a href="">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h3>DC</h3>
+                    <li>
+                        <a href="">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="">CPSC Certificates
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="">Contacts Us</a>
+                    </li>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h3>SITES</h3>
+                    <li>
+                        <a href="">DC</a>
+                    </li>
+                    <li>
+                        <a href="">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="">DC Power Visa</a>
+                    </li>
+    
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-sing-up">
+            <div class="container">
+                <div class="box-sign-up">
+                    <a href="">SIGN-UP NOW!</a>
+                </div>
+                <div class="icon-social">
+                    <h3>
+                        <a href="">FOLLOW US</a>
+                    </h3>
+                    <ul class="list-item-social">
+                        <li>
+                            <a href="">
+                            <img src="{{ Vite::asset ('resources/img/footer-facebook.png') }}" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            <img src="{{ Vite::asset ('resources/img/footer-twitter.png') }}" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            <img src="{{ Vite::asset ('resources/img/footer-youtube.png') }}" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            <img src="{{ Vite::asset ('resources/img/footer-pinterest.png') }}" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            <img src="{{ Vite::asset ('resources/img/footer-periscope.png') }}" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+        <div class="container">
+            <div class="logo">
+                <img src="{{ Vite::asset ('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <ul class="list-item-header">
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="">COMICS</a>
+                </li>
+                <li>
+                    <a href="">MOVIES</a>
+                </li>
+                <li>
+                    <a href="">TV</a>
+                </li>
+                <li>
+                    <a href="">GAMES</a>
+                </li>
+                <li>
+                    <a href="">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="">FANS</a>
+                </li>
+                <li>
+                    <a href="">NEWS</a>
+                </li>
+                <li>
+                    <a href="">SHOP</a>
+                </li>
+            </ul>
+        </div>
+        <div class="img-jumbo">
+            <img src="{{ Vite::asset ('resources/img/jumbotron.jpg') }}" alt="">
+        </div>
